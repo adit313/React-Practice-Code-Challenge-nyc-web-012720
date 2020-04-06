c9777df2227ca59dd68ab9ff01ec55bbbb1af5fe
adit313 <app2116@gmail.com>
2020-04-06 14:06:23 -0400

    All sushi components

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,11 +6,55 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushi: [],
+    startIndex: 0,
+    eatenSushi: []
+  }
+
+  addEaten = (data) => {
+    data.forEach(element => {
+      element["eaten"] = false
+    })
+    return data
+  }
+
+  nextPage = () => {
+    this.setState({startIndex: this.state.startIndex + 4})
+  }
+
+  eaten = (sushiToUpdate) => {
+
+    this.setState( prevState =>({
+      ...prevState,
+        sushi: prevState.sushi.map(piece =>{
+          if (piece === sushiToUpdate) {
+            return {...piece, eaten:true}
+          } else {
+            return piece
+          }
+          }),
+          eatenSushi: [...prevState.eatenSushi, sushiToUpdate]
+      })
+    )
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => this.setState({sushi: this.addEaten(data)}))
+  }
 
   render() {
+    console.log(this.state)
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer 
+          sushis={this.state.sushi} 
+          startIndex={this.state.startIndex} 
+          nextPage = {this.nextPage}
+          eaten={this.eaten}
+          />
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  const renderSushi = () => {
+    return props.sushis.slice(props.startIndex, (props.startIndex + 4)).map(piece=> {
+      return <Sushi 
+          key={piece.id} 
+          name={piece.name} 
+          url={piece.img_url} 
+          price={piece.price} 
+          eaten={piece.eaten}
+          clickHandle={() => props.eaten(piece)}
+          />
+    })
+  }
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          renderSushi()
         }
-        <MoreButton />
+        <MoreButton nextPage = {props.nextPage}/>
       </div>
     </Fragment>
   )
